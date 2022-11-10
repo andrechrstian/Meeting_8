256edfbb9b63dca471147c25d1365eb469de772f
andrechrstian <andrechristianacl@gmail.com>
2022-11-10 23:32:28 +0800

    add meeting 8

---
+++ index.js
@@ -0,0 +1,33 @@
+//function countDiscount (diskon) {
+//    const clothPrice = 250000
+//    const hitung = 250000-(250000*diskon/100)
+//   return hitung;
+
+//}
+
+//const Januari  = countDiscount (10)
+//const Februari = countDiscount (40)
+//const Maret    = countDiscount (70)
+
+//console.log(Januari,Februari,Maret);
+
+//Bentuk Bentuk function
+
+//function declarative
+//funcktion expression
+//function expression arrow
+
+const pangkat = (param1, param2) => {
+    const coba = Math.pow (param1, param2)
+     return coba;
+    }
+    
+    const angka = pangkat (2,3)
+    
+    console.log (angka);
+    
+    
+    
+    
+    
+    
